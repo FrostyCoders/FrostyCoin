f240f1591f54bfcc56134e1a4b78160466e73552
Jager4561 <wojtek9631@gmail.com>
2020-02-19 22:57:49 +0100

    Add users.php

--- fp-admin/users.php
+++ fp-admin/users.php
@@ -38,55 +38,53 @@
             <a href="product_categories.php"><div class="menu-element">Kategorie produktów</div></a>
             <a href="products.php"><div class="menu-element">Produkty</div></a>
             <a href="#"><div class="menu-element">Podstrony</div></a>
-            <a href="orders.php"><div class="menu-element active">Zamówienia</div></a>
-            <a href="users.php"><div class="menu-element">Użytkownicy</div></a>
+            <a href="orders.php"><div class="menu-element">Zamówienia</div></a>
+            <a href="users.php"><div class="menu-element active">Użytkownicy</div></a>
             <p class="copy">Copyright &copy;<br>Frosty Coders - 2020<br>wersja 0.1.0</p>
         </div>
         <div id="content" class="content">
             <div id="content-frame" class="content-frame">
                 <div class="content-title">
-                    Zamówienia
+                    Użytkownicy
                 </div>
                 <div class="product_filters">
                     <div class="filter_bracket">
-                        Szukaj <br>
+                        Nazwa <br>
                         <input type="text">
                     </div>
                     <div class="filter_bracket">
                         Sortuj według <br>
                         <select name="" id="">
                             <option>Nazwa</option>
-                            <option value="">Cena</option>
-                            <option value="">Data wprowadzenia</option>
+                            <option value="">Data utworzenia</option>
                         </select><br><br>
                         <select name="" id="">
                             <option value="">Rosnąco</option>
                             <option value="">Malejąco</option>
                         </select>
-                        
                     </div>
                     <div class="filter_bracket">
-                        Zamówień na stronie <br>
+                        Użytkowników na stronie <br>
                         <select name="" id="">
                             <option value="">20</option>
                             <option value="">50</option>
                             <option value="">100</option>
-                            <option value="">Wszystkie</option>
+                            <option value="">Wszyscy</option>
                         </select>
                     </div>
                     <div class="filter_bracket">
-                        Data od<br>
+                        Data rejestracji od<br>
                         <input type="date"><br>
                         Do<br>
                         <input type="date">
                     </div>
                     <div class="filter_bracket">
-                        Status <br>
+                        Rola <br>
                         <select name="" id="">
-                            <option value="">Złożono</option>
-                            <option value="">Przygotowano</option>
-                            <option value="">Wysłano</option>
-                            <option value="">Zakończono</option>
+                            <option value="">Administrator</option>
+                            <option value="">Pracownik sklepu</option>
+                            <option value="">Dostawca</option>
+                            <option value="">Klient</option>
                         </select>
                     </div>
                     <button class="accept_filters" style="margin-top: 60px;">Zastosuj filtry</button>
@@ -94,10 +92,10 @@
                 <div class="list_container">
                     <div class="list_desc" style="margin-bottom: 30px;">
                         <div class="id">Identyfikator</div>
-                        <div class="user">Użytkownik</div>
-                        <div class="date">Data zamówienia</div>
-                        <div class="status">Status</div>
-                        <div class="value">Wartość</div>
+                        <div class="user">Nazwa</div>
+                        <div class="date">Data utworzenia</div>
+                        <div class="status">Rola</div>
+                        <div class="value">Ilość wydarzeń</div>
                         <div class="empty"></div>
                     </div>
                     <div class="list">
@@ -105,8 +103,8 @@
                             <div class="id">200219123456</div>
                             <div class="user">example87</div>
                             <div class="date">19-02-2020</div>
-                            <div class="status">Wysłane</div>
-                            <div class="value">1200.00 PLN</div>
+                            <div class="status">Administrator</div>
+                            <div class="value">12</div>
                             <div class="empty">
                                 <div class="position_control" style="width: auto;">
                                     <button class="control_button">Podgląd</button>
@@ -117,8 +115,8 @@
                             <div class="id">200217654321</div>
                             <div class="user">example57</div>
                             <div class="date">17-02-2020</div>
-                            <div class="status">Zakończono</div>
-                            <div class="value">870.00 PLN</div>
+                            <div class="status">Klient</div>
+                            <div class="value">2</div>
                             <div class="empty">
                                 <div class="position_control" style="width: auto;">
                                     <button class="control_button">Podgląd</button>
