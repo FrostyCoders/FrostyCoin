bb814a69a3492ddee9ec26779fdadac83c88e269
farriven <legend4rny@gmail.com>
2020-03-08 18:32:06 +0100

    Add security to php in statements

--- fp-admin/statements_save.php
+++ fp-admin/statements_save.php
@@ -35,13 +35,15 @@ if(!empty($_POST['statement_title'] && $_POST['statement_desc'] && $_POST['state
     {
         if($activation != NULL)
         {
-            $update = "UPDATE `statements` SET `statement_title`='$title', `statement_desc`='$desc', `statement_status`=1, `statement_from`='$datefrom', `statement_to`='$dateto', `statement_creation_time`=NULL, `statement_creator`='$user' ORDER BY `statement_id` DESC LIMIT 1;";
-            $conn->query($update);
+            $update = "UPDATE `statements` SET `statement_title`= :title, `statement_desc`= :desc, `statement_status`=1, `statement_from`= :datefrom, `statement_to`= :dateto, `statement_creation_time`=NULL, `statement_creator`= :user ORDER BY `statement_id` DESC LIMIT 1;";
+            $upd = $conn->prepare($update, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+            $upd->execute(array(':title' => $title, ':desc' => $desc, ':datefrom' => $datefrom, ':dateto' => $dateto, ':user' => $user));
         }
         else if($activation == NULL)
         {
             $update = "UPDATE `statements` SET `statement_title`='$title', `statement_desc`='$desc', `statement_status`=0, `statement_from`='$datefrom', `statement_to`='$dateto', `statement_creation_time`=NULL, `statement_creator`='$user' ORDER BY `statement_id` DESC LIMIT 1;";
-            $conn->query($update);
+            $upd = $conn->prepare($update, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+            $upd->execute(array(':title' => $title, ':desc' => $desc, ':datefrom' => $datefrom, ':dateto' => $dateto, ':user' => $user));
         }
         else
         {
@@ -59,14 +61,16 @@ if(!empty($_POST['statement_title'] && $_POST['statement_desc'] && $_POST['state
         if($activation != NULL)
         {
             $insert = "INSERT INTO `statements` (`statement_id`, `statement_title`, `statement_desc`, `statement_status`, `statement_from`, `statement_to`, `statement_creation_time`, `statement_creator`)
-            VALUES (NULL, '$title', '$desc', '1', '$datefrom', '$dateto', NULL, '$user');";
-            $conn->query($insert);
+            VALUES (NULL, :title, :desc, '1', :datefrom, :dateto, NULL, :user);";
+            $ins = $conn->prepare($insert, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+            $ins->execute(array(':title' => $title, ':desc' => $desc, ':datefrom' => $datefrom, ':dateto' => $dateto, ':user' => $user));
         }
         else if($activation == NULL)
         {
             $insert = "INSERT INTO `statements` (`statement_id`, `statement_title`, `statement_desc`, `statement_status`, `statement_from`, `statement_to`, `statement_creation_time`, `statement_creator`)
             VALUES (NULL, '$title', '$desc', '0', '$datefrom', '$dateto', NULL, '$user');";
-            $conn->query($insert);
+            $ins = $conn->prepare($insert, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+            $ins->execute(array(':title' => $title, ':desc' => $desc, ':datefrom' => $datefrom, ':dateto' => $dateto, ':user' => $user));
         }
         else
         {
