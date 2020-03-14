fae66f38db9082dc3b31603c418f18af1af1d347
farriven <legend4rny@gmail.com>
2020-03-14 18:37:22 +0100

    Fix date comparison in statements_save.php and make prototype of filters in orders.php

--- fp-admin/orders.php
+++ fp-admin/orders.php
@@ -5,6 +5,163 @@
         header("Location: index.php");
         exit();
     }
+
+    require_once "connect.php";
+
+    $setnames = "SET NAMES utf8";
+    $conn->query($setnames);
+
+    if(isset($_GET['filter']))
+    {
+        $order_search = $_POST['order_search'];
+        $order_search2 = $_POST['order_search2'];
+        $order_search2 = htmlentities($order_search2, ENT_QUOTES, "UTF-8");
+        $order_status = $_POST['order_status'];
+        $order_quantity = $_POST['order_quantity'];
+        $order_sort = $_POST['order_sort'];
+        $order_display = $_POST['order_display'];
+        $order_from = $_POST['order_from'];
+        $order_to = $_POST['order_to'];
+        $sql1 = "SELECT `products`.*, `product_categories`.`category_name` FROM `products` INNER JOIN `product_categories` ON `products`.`category_id`=`product_categories`.`category_id`";
+        
+        if($product_categories>0)
+        {
+            $sql_categories_query = "SELECT `category_id`, `category_name` FROM `product_categories` WHERE `category_id`=".$product_categories.";";
+            $sql_categories_queryy = $conn->query($sql_categories_query);
+            while($categories_query = $sql_categories_queryy -> fetch())
+            {
+                $yesc = $categories_query['category_name'];
+                $sql2 = " WHERE `product_categories`.`category_name`='$yesc'";
+                $_SESSION['product_categories'] = $categories_query['category_id'];
+            }
+        }
+        else
+        {
+            $sql2 = " WHERE `product_categories`.`category_id`>0";
+            $_SESSION['product_categories'] = 0;
+        }
+        
+        switch($product_sort)
+        {
+            case 1:
+            {
+                $sql3 = " ORDER BY `products`.`product_name`";
+                $_SESSION['product_sort'] = 1;
+                break;
+            }
+            case 2:
+            {
+                $sql3 = " ORDER BY `products`.`product_amount`";
+                $_SESSION['product_sort'] = 2;
+                break;
+            }
+            case 3:
+            {
+                
+                $sql3 = " ORDER BY `products`.`product_price`";
+                $_SESSION['product_sort'] = 3;
+                break;
+            }
+            case 4:
+            {
+                $sql3 = " ORDER BY `products`.`product_from`";
+                $_SESSION['product_sort'] = 4;
+                break;
+            }
+        }
+        switch($product_quantity)
+        {
+            case 1:
+            {
+                $sql4 = " LIMIT 20;";
+                $_SESSION['product_quantity'] = 1;
+                break;
+            }
+            case 2:
+            {
+                $sql4 = " LIMIT 50;";
+                $_SESSION['product_quantity'] = 2;
+                break;
+            }
+            case 3:
+            {
+                $sql4 = " LIMIT 100;";
+                $_SESSION['product_quantity'] = 3;
+                break;
+            }
+            case 4:
+            {
+                $sql4 = ";";
+                $_SESSION['product_quantity'] = 4;
+                break;
+            }
+        }
+        switch($product_discount)
+        {
+            case 1:
+            {
+                $sql5 = " AND `products`.`product_sale`=0";
+                $_SESSION['product_discount'] = 1;
+                break;
+            }
+            case 2:
+            {
+                $sql5 = " AND `products`.`product_sale`=1";
+                $_SESSION['product_discount'] = 2;
+                break;
+            }
+            
+        }
+        
+        switch($product_display)
+        {
+            case 1:
+            {
+                $sql6 = " ASC";
+                $_SESSION['product_display'] = 1;
+                break;
+            }
+            case 2:
+            {
+                $sql6 = " DESC";
+                $_SESSION['product_display'] = 2;
+                break;
+            } 
+        }
+        
+        switch($product_status)
+        {
+            case 1:
+            {
+                $sql7 = " AND `products`.`product_status`>1";
+                $_SESSION['product_status'] = 1;
+                break;
+            }
+            case 2:
+            {
+                $sql7 = " AND `products`.`product_status`=1";
+                $_SESSION['product_status'] = 2;
+                break;
+            } 
+            case 3:
+            {
+                $sql7 = "  AND `products`.`product_status`=0";
+                $_SESSION['product_status'] = 3;
+                break;
+            } 
+        }
+        $sql_select = $sql1 . $sql2 . $sql5 . $sql7 . $sql3 . $sql6 . $sql4;
+    }
+    else
+    {
+        $sql_select = "SELECT `products`.*, `product_categories`.`category_name` FROM `products` INNER JOIN `product_categories` ON `products`.`category_id`=`product_categories`.`category_id` WHERE `product_status`>1 ORDER BY `products`.`product_name` ASC LIMIT 20;";
+        $_SESSION['product_categories'] = 0;
+        $_SESSION['product_sort'] = 0;
+        $_SESSION['product_quantity'] = 1;
+        $_SESSION['product_discount'] = 0;
+        $_SESSION['product_display'] = 1;
+        $_SESSION['product_status'] = 1;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -63,13 +220,19 @@
                     Zamówienia
                 </div>
                 <div class="product_filters">
+                <form action="orders.php?filter=1" method="post">
                     <div class="filter_bracket">
                         Szukaj <br>
-                        <input type="text">
+                        <select name="order_search">
+                            <option value="">Identyfikator</option>
+                            <option value="">Użytkownik</option>
+                            <option value="">Cena</option>
+                        </select><br>
+                        <input name="order_search2" type="text">
                     </div>
                     <div class="filter_bracket">
                         Zamówień na stronie <br>
-                        <select name="" id="">
+                        <select name="order_quantity" id="">
                             <option value="">20</option>
                             <option value="">50</option>
                             <option value="">100</option>
@@ -78,7 +241,7 @@
                     </div>
                     <div class="filter_bracket">
                         Status <br>
-                        <select name="" id="">
+                        <select name="order_status" id="">
                             <option value="">Złożono</option>
                             <option value="">Przygotowano</option>
                             <option value="">Wysłano</option>
@@ -87,12 +250,12 @@
                     </div>
                     <div class="filter_bracket">
                         Sortuj według <br>
-                        <select name="" id="">
+                        <select name="order_sort" id="">
                             <option>Nazwa</option>
                             <option value="">Cena</option>
                             <option value="">Data wprowadzenia</option>
                         </select><br><br>
-                        <select name="" id="">
+                        <select name="order_display" id="">
                             <option value="">Rosnąco</option>
                             <option value="">Malejąco</option>
                         </select>
@@ -100,11 +263,12 @@
                     </div>
                     <div class="filter_bracket filter_bracket_date">
                         Data od<br>
-                        <input type="date"><br>
+                        <input name="order_form" type="date"><br>
                         Do<br>
-                        <input type="date">
+                        <input name="order_to" type="date">
                     </div>
-                    <button class="accept_filters" style="margin-top: 60px;">Zastosuj filtry</button>
+                    <input type="submit" class="accept_filters" style="margin-top: 60px;" value="Zastosuj filtry">
+                </form>
                 </div>
                 <div class="list_container">
                     <div class="list">
--- fp-admin/statements_save.php
+++ fp-admin/statements_save.php
@@ -18,20 +18,22 @@
     $activation = $_POST['activation'];
 
     $title = $_POST['statement_title'];
-    $title = htmlentities($title, ENT_QUOTES, "UTF-8");
+    
 
     $desc = $_POST['statement_desc'];
     $desc = htmlentities($desc, ENT_QUOTES, "UTF-8");
 
     $datefrom = $_POST['statement_from'];
+    $datefromsec = strtotime($datefrom);
     $dateto = $_POST['statement_to'];
+    $datetosec = strtotime($dateto);
     $user = $_SESSION['admin_id'];
     
 // MOD
 
 if(!empty($_POST['statement_title'] && $_POST['statement_desc'] && $_POST['statement_from'] && $_POST['statement_to'])) 
 {
-    if($mod != NULL)
+    if($mod != NULL && $datetosec>=$datefromsec)
     {
         if($activation != NULL)
         {
@@ -45,7 +47,7 @@ if(!empty($_POST['statement_title'] && $_POST['statement_desc'] && $_POST['state
         {
             $inactive = "UPDATE `statements` SET `statement_status`=0";
             $inactive = $conn->query($inactive);
-            $update = "UPDATE `statements` SET `statement_title`='$title', `statement_desc`='$desc', `statement_status`=0, `statement_from`='$datefrom', `statement_to`='$dateto', `statement_creation_time`=NULL, `statement_creator`='$user' ORDER BY `statement_id` DESC LIMIT 1;";
+            $update = "UPDATE `statements` SET `statement_title`= :title, `statement_desc`= :desc, `statement_status`=0, `statement_from`= :datefrom, `statement_to`= :dateto, `statement_creation_time`=NULL, `statement_creator`= :user ORDER BY `statement_id` DESC LIMIT 1;";
             $upd = $conn->prepare($update, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
             $upd->execute(array(':title' => $title, ':desc' => $desc, ':datefrom' => $datefrom, ':dateto' => $dateto, ':user' => $user));
         }
@@ -60,7 +62,7 @@ if(!empty($_POST['statement_title'] && $_POST['statement_desc'] && $_POST['state
     }
 
 // INSERT
-    else if($mod == NULL)
+    else if($mod == NULL && $datetosec>=$datefromsec)
     {
         if($activation != NULL)
         {
@@ -76,7 +78,7 @@ if(!empty($_POST['statement_title'] && $_POST['statement_desc'] && $_POST['state
             $inactive = "UPDATE `statements` SET `statement_status`=0";
             $inactive = $conn->query($inactive);
             $insert = "INSERT INTO `statements` (`statement_id`, `statement_title`, `statement_desc`, `statement_status`, `statement_from`, `statement_to`, `statement_creation_time`, `statement_creator`)
-            VALUES (NULL, '$title', '$desc', '0', '$datefrom', '$dateto', NULL, '$user');";
+            VALUES (NULL, :title, :desc, '0', :datefrom, :dateto, NULL, :user);";
             $ins = $conn->prepare($insert, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
             $ins->execute(array(':title' => $title, ':desc' => $desc, ':datefrom' => $datefrom, ':dateto' => $dateto, ':user' => $user));
         }
