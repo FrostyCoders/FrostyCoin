a9685dfcf3794e01435360a08374af0f95a24001
Git-Gud-Dude <pl.krzysiek.pl.pl@gmail.com>
2020-03-16 20:06:54 +0100

    Change in css/main.css php_scripts/summary.php main_page.php

--- fp-admin/php_scripts/summary.php
+++ fp-admin/php_scripts/summary.php
@@ -1,71 +1,65 @@
 <?php
-session_start();
-require_once "../connect.php";
 
 //NUMBER OF CATEGORIES
 
     $sum_cat = $conn->query("SELECT COUNT(*) as sum_cat FROM product_categories");
     foreach($sum_cat as $row)
-      {
-          echo $row['sum_cat'];
-      }
-    $_SESSION['sum_cat']=$row['sum_cat'];
-echo '<br />';
+      
+   $sum_cat=$row['sum_cat'];
+
 
 //NUMBER OF ORDERS
 
     $sum_order = $conn->query("SELECT COUNT(*) as sum_order FROM order_status WHERE status_name='active'");
     foreach($sum_order as $row)
-      {
-          echo $row['sum_order'];
-      }
-    $_SESSION['sum_order']=$row['sum_order'];
-echo '<br />';
+
+    $sum_order=$row['sum_order'];
 
 //NUMBER OF PRODUCTS
 
     $sum_pro = $conn->query("SELECT COUNT(*) as sum_pro FROM products");
     foreach($sum_pro as $row)
-      {
-          echo $row['sum_pro'];
-      }
-    $_SESSION['sum_pro']=$row['sum_pro'];
-echo '<br />';
+     
+    $sum_pro=$row['sum_pro'];
+
 
 //LAST ADDED FIRST
 
     $pro_one = $conn->query("SELECT product_id, product_name, product_price FROM products ORDER BY product_from LIMIT 1");
     
 foreach($pro_one as $row)
-      {
-          echo $row['product_id'].'<br />'. $row['product_name'].'<br />'. $row['product_price'];
-      }
-    $_SESSION['pro_one_1']=$row['product_id'];
-    $_SESSION['pro_one_2']=$row['product_name'];
-    $_SESSION['pro_one_3']=$row['product_price'];
+      
+    $pro_one_1=$row['product_id'];
+    $pro_one_2=$row['product_name'];
+    $pro_one_3=$row['product_price'];
 
 //LAST ADDED SECOND
 
     $pro_one = $conn->query("SELECT product_id, product_name, product_price FROM products ORDER BY product_from LIMIT 2");
     
 foreach($pro_one as $row)
-      {
-          echo $row['product_id'].'<br />'. $row['product_name'].'<br />'. $row['product_price'];
-      }
-    $_SESSION['pro_two_1']=$row['product_id'];
-    $_SESSION['pro_two_2']=$row['product_name'];
-    $_SESSION['pro_two_3']=$row['product_price'];
+     
+    $pro_two_1=$row['product_id'];
+    $pro_two_2=$row['product_name'];
+    $pro_two_3=$row['product_price'];
 
 //LAST ADDED THIRD
 
     $pro_one = $conn->query("SELECT product_id, product_name, product_price FROM products ORDER BY product_from LIMIT 3");
     
 foreach($pro_one as $row)
-      {
-          echo $row['product_id'].'<br />'. $row['product_name'].'<br />'. $row['product_price'];
-      }
-    $_SESSION['pro_three_1']=$row['product_id'];
-    $_SESSION['pro_three_2']=$row['product_name'];
-    $_SESSION['pro_three_3']=$row['product_price'];
+      
+    $pro_three_1=$row['product_id'];
+    $pro_three_2=$row['product_name'];
+    $pro_three_3=$row['product_price'];
+
+//DISK USAGE CHART
+
+$disc_space = round(disk_total_space("/")/1024/1024/1024);
+$disc_free = round(disk_free_space("/")/1024/1024/1024,2);
+$disc_used = $disc_space-$disc_free;
+
+$disc_precent = round(($disc_used /$disc_space)*100,2);
+$chart = $disc_precent*3.6;    
 
 ?>
